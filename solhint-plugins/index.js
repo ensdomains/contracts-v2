9f623cac4568632aef45a1aff100befc3a5a743b
tate <tate@ens.domains>
2026-02-24 13:47:00 +1100

    misc: solhint selector tags plugin

--- solhint-plugins/index.js
+++ solhint-plugins/index.js
@@ -1,4 +1,5 @@
 const ordering = require("./ordering");
 const importOrderSeparation = require("./import-order-separation");
+const selectorTags = require("./selector-tags");
 
-module.exports = [ordering, importOrderSeparation];
+module.exports = [ordering, importOrderSeparation, selectorTags];
