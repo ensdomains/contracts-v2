4212f227a8c19db6bb23072054e13fb7cf089399
Andrew Raffensperger <raffy@me.com>
2026-02-25 16:27:24 -0800

    fix comments

--- contracts/src/migration/MigrationErrors.sol
+++ contracts/src/migration/MigrationErrors.sol
@@ -3,15 +3,15 @@ pragma solidity >=0.8.13;
 
 /// @dev Errors for migration process.
 library MigrationErrors {
-    /// @notice Name cannot be registered because umigrated NameWrapper token exists.
+    /// @notice Name cannot be registered because unmigrated NameWrapper token exists.
     /// @dev Error selector: `0x408fa1b8`
     error NameRequiresMigration();
 
-    /// @notice NameWrapper was not locked.
+    /// @notice NameWrapper token was not locked.
     /// @dev Error selector: `0x1bfe8f0a`
     error NameNotLocked(uint256 tokenId);
 
-    /// @notice NameWrapper token and supplied data don't match.
+    /// @notice NameWrapper token does not match supplied data.
     /// @dev Error selector: `0xedec3569`
     error NameDataMismatch(uint256 tokenId);
 }
