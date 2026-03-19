7f1788fb448aa78dc9a9823f933d5655a051cf74
zeroknots <zeroknots@protonmail.com>
2026-03-19 16:19:07 +0700

    selector

--- contracts/src/hca/interfaces/IHCAInitDataParser.sol
+++ contracts/src/hca/interfaces/IHCAInitDataParser.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.27;
 
 /// @title IHCAInitDataParser
+/// @dev Interface selector: `0xf9660ea1`
 interface IHCAInitDataParser {
     function getOwnerFromInitData(bytes calldata initData) external view returns (address hcaOwner);
 }
