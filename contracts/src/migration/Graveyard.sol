6adaf36a77c2cf10a6255ba6b9403c1f76653886
Andrew Raffensperger <raffy@me.com>
2026-04-10 15:49:32 -0700

    refactor to just clear()

--- contracts/src/migration/Graveyard.sol
+++ contracts/src/migration/Graveyard.sol
@@ -1,6 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.13;
 
+import {
+    BaseRegistrarImplementation
+} from "@ens/contracts/ethregistrar/BaseRegistrarImplementation.sol";
 import {IBaseRegistrar} from "@ens/contracts/ethregistrar/IBaseRegistrar.sol";
 import {ENS} from "@ens/contracts/registry/ENS.sol";
 import {NameCoder} from "@ens/contracts/utils/NameCoder.sol";
@@ -8,12 +11,30 @@ import {INameWrapper} from "@ens/contracts/wrapper/INameWrapper.sol";
 import {ERC1155Holder} from "@openzeppelin/contracts/token/ERC1155/utils/ERC1155Holder.sol";
 import {ERC721Holder} from "@openzeppelin/contracts/token/ERC721/utils/ERC721Holder.sol";
 
+import {LibMigration} from "./libraries/LibMigration.sol";
+
 /// @notice The ENSv1 ETHRegistrarController for ENSv2 launch which becomes the burn address for migrated tokens.
 ///
 /// 1. Claim any expired ENSv1 name and assign ownership to this contract.
 /// 2. Clear the registry for any owned token.
 ///
 contract Graveyard is ERC721Holder, ERC1155Holder {
+    ////////////////////////////////////////////////////////////////////////
+    // Types
+    ////////////////////////////////////////////////////////////////////////
+
+    /// @dev The internal states of registry ownership.
+    enum State {
+        ROOT,
+        ETH,
+        OWNED,
+        LOCKED
+    }
+
+    ////////////////////////////////////////////////////////////////////////
+    // Constants
+    ////////////////////////////////////////////////////////////////////////
+
     /// @notice The ENSv1 `NameWrapper` contract.
     INameWrapper public immutable NAME_WRAPPER;
 
@@ -22,6 +43,9 @@ contract Graveyard is ERC721Holder, ERC1155Holder {
 
     /// @dev The ENSv1 `BaseRegistrar` contract.
     IBaseRegistrar internal immutable _REGISTRAR_V1;
+
+    /// @dev Same as `BaseRegistrarImplementation.GRACE_PERIOD()`.
+    uint256 internal immutable _GRACE_PERIOD;
 
     ////////////////////////////////////////////////////////////////////////
     // Initialization
@@ -33,57 +57,87 @@ contract Graveyard is ERC721Holder, ERC1155Holder {
         NAME_WRAPPER = nameWrapper;
         _REGISTRY_V1 = nameWrapper.ens();
         _REGISTRAR_V1 = nameWrapper.registrar();
+        _GRACE_PERIOD = BaseRegistrarImplementation(address(_REGISTRAR_V1)).GRACE_PERIOD();
     }
 
     ////////////////////////////////////////////////////////////////////////
     // Implementation
     ////////////////////////////////////////////////////////////////////////
 
-    /// @notice Clear registry for unwrapped names with automatic temporary registration.
-    /// @param parentNodes The array of unwrapped parent nodes.
-    /// @param labelHashes The array of child labelhashes.
-    function clearUnwrapped(
-        bytes32[] calldata parentNodes,
-        bytes32[] calldata labelHashes
-    ) external {
-        for (uint256 i; i < parentNodes.length; ++i) {
-            bytes32 parentNode = parentNodes[i];
-            bytes32 labelHash = labelHashes[i];
-            if (parentNode == NameCoder.ETH_NODE) {
-                bytes32 node = NameCoder.namehash(parentNode, labelHash);
-                if (_REGISTRY_V1.owner(node) != address(this)) {
-                    _REGISTRAR_V1.register(uint256(labelHash), address(this), 1);
-                }
-                _REGISTRY_V1.setResolver(node, address(0));
-            } else {
-                _REGISTRY_V1.setSubnodeRecord(parentNode, labelHash, address(this), address(0), 0);
-            }
+    /// @notice Clear registry for migrated names.
+    /// @param names The array of names to clear.
+    function clear(bytes[] calldata names) external {
+        for (uint256 i; i < names.length; ++i) {
+            _clear(names[i], 0);
         }
     }
 
-    /// @notice Clear registry for wrapped children with automatic unwrap.
-    /// @param parentNodes The array of wrapped parent nodes.
-    /// @param labels The array of child labels.
-    function clearWrappedChildren(
-        bytes32[] calldata parentNodes,
-        string[] calldata labels
-    ) external {
-        for (uint256 i; i < parentNodes.length; ++i) {
-            bytes32 parentNode = parentNodes[i];
-            string calldata label = labels[i];
-            //bytes32 node = NAME_WRAPPER.setSubnodeRecord(
+    ////////////////////////////////////////////////////////////////////////
+    // Internal Functions
+    ////////////////////////////////////////////////////////////////////////
+
+    /// @dev Recursively clear ancestor namespace.
+    function _clear(
+        bytes calldata name,
+        uint256 offset
+    ) internal returns (bytes32 node, State state) {
+        (bytes32 labelHash, uint256 nextOffset) = NameCoder.readLabel(name, offset);
+        if (labelHash == bytes32(0)) {
+            return (bytes32(0), State.ROOT);
+        }
+        bytes32 parentNode;
+        (parentNode, state) = _clear(name, nextOffset);
+        node = NameCoder.namehash(parentNode, labelHash);
+        if (state == State.ROOT) {
+            require(node == NameCoder.ETH_NODE);
+            return (node, State.ETH);
+        } else if (state == State.ETH) {
+            address owner = _REGISTRY_V1.owner(node);
+            if (owner == address(this)) {
+                // resolver is cleared by migration
+                return (node, State.OWNED);
+            }
+            uint32 fuses;
+            (owner, fuses, ) = NAME_WRAPPER.getData(uint256(node));
+            if (LibMigration.isLocked(fuses)) {
+                require(owner == address(this));
+                // resolver is cleared by migration
+                return (node, State.LOCKED);
+            }
+            _REGISTRAR_V1.register(
+                uint256(labelHash),
+                address(this),
+                type(uint64).max - block.timestamp - _GRACE_PERIOD // max duration?
+            );
+            // lock expired? so clear it
+            if (_REGISTRY_V1.resolver(node) != address(0)) {
+                _REGISTRY_V1.setResolver(node, address(0));
+            }
+            return (node, State.OWNED);
+        } else if (state == State.OWNED) {
+            _REGISTRY_V1.setSubnodeRecord(parentNode, labelHash, address(this), address(0), 0);
+            return (node, State.OWNED);
+        } else {
+            (address owner, uint32 fuses, ) = NAME_WRAPPER.getData(uint256(node));
+            if (owner == address(this)) {
+                // resolver is cleared by migration
+                if (LibMigration.isLocked(fuses)) {
+                    return (node, State.LOCKED);
+                } else if (LibMigration.isEmancipatedChild(fuses)) {
+                    return (node, State.OWNED);
+                }
+            }
             NAME_WRAPPER.setSubnodeRecord(
                 parentNode,
-                label,
+                string(name[offset + 1:nextOffset]),
                 address(this), // owner
                 address(0), // resolver
                 0, // ttl
                 0, // fuses
-                0 // expiry
-            );
-            //(, uint32 fuses, ) = NAME_WRAPPER.getData(uint256(node));
-            //if (!LibMigration.isLocked(fuses)) {
-            NAME_WRAPPER.unwrap(parentNode, keccak256(bytes(label)), address(this));
+                0 // expiry (uses min)
+            ); // reverts if not migrated
+            NAME_WRAPPER.unwrap(parentNode, labelHash, address(this));
+            return (node, State.OWNED);
         }
     }
 }
