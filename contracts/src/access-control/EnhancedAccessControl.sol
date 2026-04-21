8f54092f3f918210dd8615986801e112c9a3fb12
tate <tate@ens.domains>
2026-04-21 13:13:05 -0400

    chore: apply solgrid linting

--- contracts/src/access-control/EnhancedAccessControl.sol
+++ contracts/src/access-control/EnhancedAccessControl.sol
@@ -104,9 +104,13 @@ abstract contract EnhancedAccessControl is HCAContext, ERC165, IEnhancedAccessCo
     ////////////////////////////////////////////////////////////////////////
 
     /// @inheritdoc IERC165
-    function supportsInterface(
-        bytes4 interfaceId
-    ) public view virtual override(ERC165, IERC165) returns (bool) {
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        virtual
+        override(ERC165, IERC165)
+        returns (bool)
+    {
         return
             interfaceId == type(IEnhancedAccessControl).interfaceId ||
             super.supportsInterface(interfaceId);
@@ -119,11 +123,12 @@ abstract contract EnhancedAccessControl is HCAContext, ERC165, IEnhancedAccessCo
     /// @inheritdoc IEnhancedAccessControl
     /// @dev The caller must have all the necessary admin roles for the roles being granted.
     ///      Cannot be used with ROOT_RESOURCE directly, use grantRootRoles instead.
-    function grantRoles(
-        uint256 resource,
-        uint256 roleBitmap,
-        address account
-    ) public virtual canGrantRoles(resource, roleBitmap) returns (bool) {
+    function grantRoles(uint256 resource, uint256 roleBitmap, address account)
+        public
+        virtual
+        canGrantRoles(resource, roleBitmap)
+        returns (bool)
+    {
         if (resource == ROOT_RESOURCE) {
             revert EACRootResourceNotAllowed();
         }
@@ -132,21 +137,24 @@ abstract contract EnhancedAccessControl is HCAContext, ERC165, IEnhancedAccessCo
 
     /// @inheritdoc IEnhancedAccessControl
     /// @dev The caller must have all the necessary admin roles for the roles being granted.
-    function grantRootRoles(
-        uint256 roleBitmap,
-        address account
-    ) public virtual canGrantRoles(ROOT_RESOURCE, roleBitmap) returns (bool) {
+    function grantRootRoles(uint256 roleBitmap, address account)
+        public
+        virtual
+        canGrantRoles(ROOT_RESOURCE, roleBitmap)
+        returns (bool)
+    {
         return _grantRoles(ROOT_RESOURCE, roleBitmap, account, true);
     }
 
     /// @inheritdoc IEnhancedAccessControl
     /// @dev The caller must have all the necessary admin roles for the roles being revoked.
     ///      Cannot be used with ROOT_RESOURCE directly, use revokeRootRoles instead.
-    function revokeRoles(
-        uint256 resource,
-        uint256 roleBitmap,
-        address account
-    ) public virtual canRevokeRoles(resource, roleBitmap) returns (bool) {
+    function revokeRoles(uint256 resource, uint256 roleBitmap, address account)
+        public
+        virtual
+        canRevokeRoles(resource, roleBitmap)
+        returns (bool)
+    {
         if (resource == ROOT_RESOURCE) {
             revert EACRootResourceNotAllowed();
         }
@@ -155,10 +163,12 @@ abstract contract EnhancedAccessControl is HCAContext, ERC165, IEnhancedAccessCo
 
     /// @inheritdoc IEnhancedAccessControl
     /// @dev The caller must have all the necessary admin roles for the roles being revoked.
-    function revokeRootRoles(
-        uint256 roleBitmap,
-        address account
-    ) public virtual canRevokeRoles(ROOT_RESOURCE, roleBitmap) returns (bool) {
+    function revokeRootRoles(uint256 roleBitmap, address account)
+        public
+        virtual
+        canRevokeRoles(ROOT_RESOURCE, roleBitmap)
+        returns (bool)
+    {
         return _revokeRoles(ROOT_RESOURCE, roleBitmap, account, true);
     }
 
@@ -178,11 +188,12 @@ abstract contract EnhancedAccessControl is HCAContext, ERC165, IEnhancedAccessCo
     }
 
     /// @inheritdoc IEnhancedAccessControl
-    function hasRoles(
-        uint256 resource,
-        uint256 roleBitmap,
-        address account
-    ) public view virtual returns (bool) {
+    function hasRoles(uint256 resource, uint256 roleBitmap, address account)
+        public
+        view
+        virtual
+        returns (bool)
+    {
         return
             (_roles[ROOT_RESOURCE][account] | _roles[resource][account]) & roleBitmap == roleBitmap;
     }
@@ -194,10 +205,12 @@ abstract contract EnhancedAccessControl is HCAContext, ERC165, IEnhancedAccessCo
     }
 
     /// @inheritdoc IEnhancedAccessControl
-    function getAssigneeCount(
-        uint256 resource,
-        uint256 roleBitmap
-    ) public view virtual returns (uint256 counts, uint256 mask) {
+    function getAssigneeCount(uint256 resource, uint256 roleBitmap)
+        public
+        view
+        virtual
+        returns (uint256 counts, uint256 mask)
+    {
         mask = _roleBitmapToMask(roleBitmap);
         counts = _roleCount[resource] & mask;
     }
@@ -222,7 +235,10 @@ abstract contract EnhancedAccessControl is HCAContext, ERC165, IEnhancedAccessCo
         address srcAccount,
         address dstAccount,
         bool executeCallbacks
-    ) internal virtual {
+    )
+        internal
+        virtual
+    {
         uint256 srcRoles = _roles[resource][srcAccount];
         if (srcRoles != 0) {
             // First revoke roles from source account to free up assignee slots
@@ -243,7 +259,11 @@ abstract contract EnhancedAccessControl is HCAContext, ERC165, IEnhancedAccessCo
         uint256 roleBitmap,
         address account,
         bool executeCallbacks
-    ) internal virtual returns (bool) {
+    )
+        internal
+        virtual
+        returns (bool)
+    {
         if (roleBitmap == 0) {
             return false;
         }
@@ -279,7 +299,11 @@ abstract contract EnhancedAccessControl is HCAContext, ERC165, IEnhancedAccessCo
         uint256 roleBitmap,
         address account,
         bool executeCallbacks
-    ) internal virtual returns (bool) {
+    )
+        internal
+        virtual
+        returns (bool)
+    {
         _checkRoleBitmap(roleBitmap);
         uint256 currentRoles = _roles[resource][account];
         uint256 updatedRoles = currentRoles & ~roleBitmap;
@@ -299,11 +323,11 @@ abstract contract EnhancedAccessControl is HCAContext, ERC165, IEnhancedAccessCo
     }
 
     /// @dev Revoke all roles for account within resource.
-    function _revokeAllRoles(
-        uint256 resource,
-        address account,
-        bool executeCallbacks
-    ) internal virtual returns (bool) {
+    function _revokeAllRoles(uint256 resource, address account, bool executeCallbacks)
+        internal
+        virtual
+        returns (bool)
+    {
         return _revokeRoles(resource, EACBaseRolesLib.ALL_ROLES, account, executeCallbacks);
     }
 
@@ -341,7 +365,10 @@ abstract contract EnhancedAccessControl is HCAContext, ERC165, IEnhancedAccessCo
         uint256 oldRoles,
         uint256 newRoles,
         uint256 roleBitmap
-    ) internal virtual {
+    )
+        internal
+        virtual
+    {
         // solhint-disable-previous-line no-empty-blocks
     }
 
@@ -357,27 +384,30 @@ abstract contract EnhancedAccessControl is HCAContext, ERC165, IEnhancedAccessCo
         uint256 oldRoles,
         uint256 newRoles,
         uint256 roleBitmap
-    ) internal virtual {
+    )
+        internal
+        virtual
+    {
         // solhint-disable-previous-line no-empty-blocks
     }
 
     /// @dev Reverts if `account` does not have all the given roles.
-    function _checkRoles(
-        uint256 resource,
-        uint256 roleBitmap,
-        address account
-    ) internal view virtual {
+    function _checkRoles(uint256 resource, uint256 roleBitmap, address account)
+        internal
+        view
+        virtual
+    {
         if (!hasRoles(resource, roleBitmap, account)) {
             revert EACUnauthorizedAccountRoles(resource, roleBitmap, account);
         }
     }
 
     /// @dev Reverts if `account` does not have the admin roles for all the given roles.
-    function _checkCanGrantRoles(
-        uint256 resource,
-        uint256 roleBitmap,
-        address account
-    ) internal view virtual {
+    function _checkCanGrantRoles(uint256 resource, uint256 roleBitmap, address account)
+        internal
+        view
+        virtual
+    {
         uint256 settableRoles = _getSettableRoles(resource, account);
         if ((roleBitmap & ~settableRoles) != 0) {
             revert EACCannotGrantRoles(resource, roleBitmap, account);
@@ -385,11 +415,11 @@ abstract contract EnhancedAccessControl is HCAContext, ERC165, IEnhancedAccessCo
     }
 
     /// @dev Reverts if `account` does not have the admin roles for all the given roles that are being revoked.
-    function _checkCanRevokeRoles(
-        uint256 resource,
-        uint256 roleBitmap,
-        address account
-    ) internal view virtual {
+    function _checkCanRevokeRoles(uint256 resource, uint256 roleBitmap, address account)
+        internal
+        view
+        virtual
+    {
         uint256 revokableRoles = _getRevokableRoles(resource, account);
         if ((roleBitmap & ~revokableRoles) != 0) {
             revert EACCannotRevokeRoles(resource, roleBitmap, account);
@@ -405,11 +435,14 @@ abstract contract EnhancedAccessControl is HCAContext, ERC165, IEnhancedAccessCo
     /// @param resource The resource to get settable roles for.
     /// @param account The account to get settable roles for.
     /// @return The settable roles for `account` within `resource`.
-    function _getSettableRoles(
-        uint256 resource,
-        address account
-    ) internal view virtual returns (uint256) {
-        uint256 adminRoleBitmap = (_roles[resource][account] | _roles[ROOT_RESOURCE][account]) &
+    function _getSettableRoles(uint256 resource, address account)
+        internal
+        view
+        virtual
+        returns (uint256)
+    {
+        uint256 adminRoleBitmap =
+            (_roles[resource][account] | _roles[ROOT_RESOURCE][account]) &
             EACBaseRolesLib.ADMIN_ROLES;
         return (adminRoleBitmap >> 128) | adminRoleBitmap;
     }
@@ -421,11 +454,14 @@ abstract contract EnhancedAccessControl is HCAContext, ERC165, IEnhancedAccessCo
     /// @param resource The resource to get revokable roles for.
     /// @param account The account to get revokable roles for.
     /// @return The revokable roles for `account` within `resource`.
-    function _getRevokableRoles(
-        uint256 resource,
-        address account
-    ) internal view virtual returns (uint256) {
-        uint256 adminRoleBitmap = (_roles[resource][account] | _roles[ROOT_RESOURCE][account]) &
+    function _getRevokableRoles(uint256 resource, address account)
+        internal
+        view
+        virtual
+        returns (uint256)
+    {
+        uint256 adminRoleBitmap =
+            (_roles[resource][account] | _roles[ROOT_RESOURCE][account]) &
             EACBaseRolesLib.ADMIN_ROLES;
         uint256 regularRoles = adminRoleBitmap >> 128;
         return regularRoles | adminRoleBitmap;
