dabcb1c709b777110b66f600879930e8ef4400c9
Andrew Raffensperger <raffy@me.com>
2026-04-30 04:17:28 -0700

    fix lint

--- contracts/src/registrar/ETHRenewerV1.sol
+++ contracts/src/registrar/ETHRenewerV1.sol
@@ -1,12 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.13;
 
-import {ERC165} from "@openzeppelin/contracts/utils/introspection/ERC165.sol";
 import {
     BaseRegistrarImplementation,
     IBaseRegistrar
 } from "@ens/contracts/ethregistrar/BaseRegistrarImplementation.sol";
 import {INameWrapper} from "@ens/contracts/wrapper/INameWrapper.sol";
+import {ERC165} from "@openzeppelin/contracts/utils/introspection/ERC165.sol";
 
 import {HCAContext} from "../hca/HCAContext.sol";
 import {HCAEquivalence} from "../hca/HCAEquivalence.sol";
