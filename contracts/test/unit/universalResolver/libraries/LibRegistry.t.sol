3b4e19aaf1576ff56439767c88b118affdf05c83
Andrew Raffensperger <raffy@me.com>
2026-05-13 14:23:21 -0700

    Add `IUniversalResolverV2` (#267)
    - added `IUniversalResolverV2`
    - updated `UniversalResolverV2`
         * restored `findParentRegistry()`
         * added `IUniversalResolverV2`
    - added `UniversalResolverV2.t.sol`

--- contracts/test/unit/universalResolver/libraries/LibRegistry.t.sol
+++ contracts/test/unit/universalResolver/libraries/LibRegistry.t.sol
@@ -7,53 +7,22 @@ import {Test} from "forge-std/Test.sol";
 
 import {ERC1155Holder} from "@openzeppelin/contracts/token/ERC1155/utils/ERC1155Holder.sol";
 import {ERC165Checker} from "@openzeppelin/contracts/utils/introspection/ERC165Checker.sol";
+import {NameCoder} from "@ens/contracts/utils/NameCoder.sol";
 
 import {EACBaseRolesLib} from "~src/access-control/EnhancedAccessControl.sol";
 import {IHCAFactoryBasic} from "~src/hca/interfaces/IHCAFactoryBasic.sol";
-import {
-    PermissionedRegistry,
-    IStandardRegistry,
-    IRegistry,
-    IRegistryMetadata
-} from "~src/registry/PermissionedRegistry.sol";
-import {LibRegistry, NameCoder} from "~src/universalResolver/libraries/LibRegistry.sol";
+import {IStandardRegistry} from "~src/registry/interfaces/IStandardRegistry.sol";
+import {IRegistry} from "~src/registry/interfaces/IRegistry.sol";
+import {IRegistryMetadata} from "~src/registry/interfaces/IRegistryMetadata.sol";
+import {PermissionedRegistry} from "~src/registry/PermissionedRegistry.sol";
+import {LibRegistry} from "~src/universalResolver/libraries/LibRegistry.sol";
 import {LabelStore} from "~src/utils/LabelStore.sol";
 
 contract LibRegistryTest is Test, ERC1155Holder {
     PermissionedRegistry rootRegistry;
     LabelStore labelStore;
-    address resolverAddress = makeAddr("resolver");
 
-    function _createRegistry() internal returns (PermissionedRegistry) {
-        return
-            new PermissionedRegistry(
-                IHCAFactoryBasic(address(0)),
-                IRegistryMetadata(address(0)),
-                labelStore,
-                address(this),
-                EACBaseRolesLib.ALL_ROLES
-            );
-    }
-    function _register(
-        PermissionedRegistry parentRegistry,
-        string memory label,
-        IRegistry registry,
-        address resolver
-    )
-        internal
-    {
-        parentRegistry.register(
-            label,
-            address(this),
-            registry,
-            resolver,
-            EACBaseRolesLib.ALL_ROLES,
-            uint64(block.timestamp + 1000)
-        );
-        if (ERC165Checker.supportsInterface(address(registry), type(IStandardRegistry).interfaceId)) {
-            IStandardRegistry(address(registry)).setParent(parentRegistry, label);
-        }
-    }
+    address resolverAddress = makeAddr("resolver");
 
     function setUp() external {
         labelStore = new LabelStore();
@@ -326,5 +295,41 @@ contract LibRegistryTest is Test, ERC1155Holder {
             address(0),
             "xyz:test.eth"
         );
+    }
+
+    ////////////////////////////////////////////////////////////////////////
+    // Helpers
+    ////////////////////////////////////////////////////////////////////////
+
+    function _createRegistry() internal returns (PermissionedRegistry) {
+        return
+            new PermissionedRegistry(
+                IHCAFactoryBasic(address(0)),
+                IRegistryMetadata(address(0)),
+                labelStore,
+                address(this),
+                EACBaseRolesLib.ALL_ROLES
+            );
+    }
+
+    function _register(
+        PermissionedRegistry parentRegistry,
+        string memory label,
+        IRegistry registry,
+        address resolver
+    )
+        internal
+    {
+        parentRegistry.register(
+            label,
+            address(this),
+            registry,
+            resolver,
+            EACBaseRolesLib.ALL_ROLES,
+            uint64(block.timestamp + 1000)
+        );
+        if (ERC165Checker.supportsInterface(address(registry), type(IStandardRegistry).interfaceId)) {
+            IStandardRegistry(address(registry)).setParent(parentRegistry, label);
+        }
     }
 }
