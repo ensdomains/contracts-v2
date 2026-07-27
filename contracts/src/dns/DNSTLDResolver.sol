5a5793c6cb16deb00d5b929bd3abd59b5f68d609
Andrew Raffensperger <raffy@me.com>
2026-07-27 13:55:33 -0700

    update comment

--- contracts/src/dns/DNSTLDResolver.sol
+++ contracts/src/dns/DNSTLDResolver.sol
@@ -356,8 +356,8 @@ contract DNSTLDResolver is
         bytes memory name = NameCoder.encode(string(v));
         (, address r, , ) = LibRegistry.findResolver(ROOT_REGISTRY, name, 0);
         if (r != address(0)) {
-            // according to ENSv1, this was immediate onchain only
-            // however this should support IExtendedResolver too
+            // in ENSv1, this was immediate only, but now supports IExtendedResolver
+            // does not support offchain
             try this.callResolver(
                 r,
                 name,
