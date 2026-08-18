ef0128feec6f8172555c54db7cafae619618fe87
Andrew Raffensperger <raffy@me.com>
2026-08-17 22:41:28 -0700

    pin UniversalHelper solc

--- contracts/src/universalResolver/UniversalHelper.sol
+++ contracts/src/universalResolver/UniversalHelper.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.8.13;
+pragma solidity 0.8.25;
 
 import {IPermissionedRegistry} from "../registry/interfaces/IPermissionedRegistry.sol";
 import {IRegistry} from "../registry/interfaces/IRegistry.sol";
